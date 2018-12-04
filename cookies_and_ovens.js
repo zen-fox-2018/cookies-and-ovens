c7d4e926da2b5bb9e94d572a1969e1ee609b8c4b
maitreya <bluespider529@gmail.com>
2018-12-05 00:00:16 +0700

    cookies and ovens

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,69 @@
 //
 //
 // Your code here
+
+class Ovens {
+    constructor(name, time) {
+        this.name = name;
+        this.time = time;
+    }
+}
+
+class Cookies {
+
+    static bake(input) {
+        let treats = input.name;
+        let time = input.time;
+
+        if(treats === "Chocolate Cake") {
+            for(let i = 0; i <= time; i+= 3) {
+                // console.log(i)
+                if(i === 3) {
+                    console.log(`${treats}, menit ke ${i} : mentah`)
+                } else if (i > 3 && i <= 12) {
+                    console.log(`${treats}, menit ke ${i}: hampir matang`)
+                } else if (i === 15) {
+                    console.log(`${treats}, menit ke ${i}: matang`)
+                } else if (i> 15){
+                    console.log(`${treats} sudah hangus :(`)
+                }
+            }
+        } else if (treats === "Peanut Cake") {
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            for(let i = 0; i <= time; i+= 5) {
+                // console.log(i)
+                if(i === 5) {
+                    console.log(`${treats}, menit ke ${i}: mentah`)
+                } else if (i > 5 && i <= 15) {
+                    console.log(`${treats}, menit ke ${i}: hampir matang`)
+                } else if (i === 20) {
+                    console.log(`${treats}, menit ke ${i}: matang`)
+                } else if (i > 20 ) {
+                    console.log(`${treats} sudah hangus :(`)
+                }
+            }
+        } else if (treats === "Cheese Cake") {
+            console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            for(let i = 0; i <= time; i+= 4) {
+                // console.log(i)
+                if(i >= 4 && i <= 20) {
+                    console.log(`${treats}, menit ke ${i}: mentah`)
+                } else if (i >= 20 && i<= 24) {
+                    console.log(`${treats}, menit ke ${i}: hampir matang`)
+                } else if (i === 28) {
+                    console.log(`${treats}, menit ke ${i}: matang`)
+                } else if (i >= 28) {
+                    console.log(`${treats} sudah hangus :(`)
+                }
+            }
+        }
+    }
+}
+
+let chocoCake = new Ovens("Chocolate Cake", 20);
+let peanutCake = new Ovens("Peanut Cake", 30);
+let cheeseCake = new Ovens("Cheese Cake", 35);
+// console.log(Muffins)
+Cookies.bake(chocoCake)
+Cookies.bake(peanutCake)
+Cookies.bake(cheeseCake)
